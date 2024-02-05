ba9e25450d3ee90e689bb1ad09e58597b25366f5
Connor Kilgore <connor_kilgore@Connors-Mac-mini.local>
2024-02-05 14:26:50 -0700

    Solved problem 2

--- spec/euler/level1/problem002_spec.clj
+++ spec/euler/level1/problem002_spec.clj
@@ -8,7 +8,12 @@
 (describe "Euler Problem #2"
 
   (it "Solves #2"
-    (should= -1 (euler-2 -1)))
-  )
+    (should= 0 (euler-2 -1))
+    (should= 0 (euler-2 1))
+    (should= 2 (euler-2 2))
+    (should= 2 (euler-2 3))
+    (should= 26658145586 (euler-2 50))
+      )
+    )
 
 (run-specs)
