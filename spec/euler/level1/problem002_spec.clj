e93e529ca037dde40147e24ebbefc9eee3a7fd5f
Connor Kilgore <connor_kilgore@Connors-Mac-mini.local>
2024-02-08 13:04:48 -0700

    massively improved euler 2

--- spec/euler/level1/problem002_spec.clj
+++ spec/euler/level1/problem002_spec.clj
@@ -7,15 +7,35 @@
 
 (describe "Euler Problem #2"
 
-  (it "Solves #2"
-    (should= 0 (euler-2 -1))
-    (should= 0 (euler-2 0))
-    (should= 0 (euler-2 1))
-    (should= 2 (euler-2 2))
-    (should= 2 (euler-2 3))
-    (should= 44 (euler-2 10))
-    (should= 26658145586 (euler-2 50))
+  (it "finds the next value in the fibonnaci sequence and appends to coll"
+    (should= nil (append-next-fibonnaci-sequence []))
+    (should= nil (append-next-fibonnaci-sequence [1]))
+    (should= [1 2 3] (append-next-fibonnaci-sequence [1 2]))
+    (should= [1 2 3 5] (append-next-fibonnaci-sequence [1 2 3]))
+    (should= [1 2 3 5 8] (append-next-fibonnaci-sequence [1 2 3 5]))
+    (should= [1 2 3 5 8 13] (append-next-fibonnaci-sequence [1 2 3 5 8]))
     )
+
+  (it "returns a fibonnaci sequence up to n"
+    (should= nil (get-fibonnaci-sequence-to-n 0))
+    (should= nil (get-fibonnaci-sequence-to-n -1))
+    (should= [1] (get-fibonnaci-sequence-to-n 1))
+    (should= [1 2] (get-fibonnaci-sequence-to-n 2))
+    (should= [1 2 3] (get-fibonnaci-sequence-to-n 3))
+    (should= [1 2 3 5] (get-fibonnaci-sequence-to-n 4))
+    (should= [1 2 3 5 8] (get-fibonnaci-sequence-to-n 5))
+    (should= [1 2 3 5 8 13 21 34] (get-fibonnaci-sequence-to-n 8))
+    )
+
+  (it "Solves #2"
+      (should= 0 (euler-2 -1))
+      (should= 0 (euler-2 0))
+      (should= 0 (euler-2 1))
+      (should= 2 (euler-2 2))
+      (should= 2 (euler-2 3))
+      (should= 44 (euler-2 10))
+      (should= 26658145586 (euler-2 50))
+      )
   )
 
 (run-specs)
--- src/euler/level1/problem002.clj
+++ src/euler/level1/problem002.clj
@@ -1,7 +1,15 @@
 (ns euler.level1.problem002)
 
+(defn append-next-fibonnaci-sequence [n]
+  (if (< (count n) 2) nil (conj n (+ (last n) (nth n (- (count n) 2))))))
+
+(defn get-fibonnaci-sequence-to-n [n]
+  (cond (< n 1) nil
+        (< n 3) (range 1 (inc n))
+        :else (loop [f-sequence [1 2]]
+          (if (>= (count f-sequence) n)
+            f-sequence (recur (append-next-fibonnaci-sequence f-sequence))))))
+
 (defn euler-2 [n]
-  (loop [index 2 totalEven 0 firstVal 1 secVal 2]
-    (cond (> index n) totalEven
-          (even? secVal) (recur (inc index) (+ totalEven secVal) secVal (+ firstVal secVal))
-          :else (recur (inc index) totalEven secVal (+ firstVal secVal)))))
+  (reduce (fn [sum f-number] (if (even? f-number) (+ sum f-number) sum))
+          0 (get-fibonnaci-sequence-to-n n)))
