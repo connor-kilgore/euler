414741a078cc294c25862d22306eb7a2f9254f49
Connor Kilgore <connor_kilgore@Connors-Mac-mini.local>
2024-02-08 13:29:09 -0700

    cleaned up problem 3

--- spec/euler/level1/problem003_spec.clj
+++ spec/euler/level1/problem003_spec.clj
@@ -8,24 +8,22 @@
 (describe "Euler Problem #3"
 
   (it "finds smallest prime factor"
-      (should= 1 (smallest-prime-factor 1))
-      (should= 1 (smallest-prime-factor -1))
-      (should= 2 (smallest-prime-factor 10))
-      (should= 2 (smallest-prime-factor 4))
-      (should= 3 (smallest-prime-factor 9))
-      (should= 13 (smallest-prime-factor 13))
-      )
+    (should= 1 (smallest-prime-factor 1))
+    (should= 1 (smallest-prime-factor -1))
+    (should= 2 (smallest-prime-factor 10))
+    (should= 2 (smallest-prime-factor 4))
+    (should= 3 (smallest-prime-factor 9))
+    (should= 13 (smallest-prime-factor 13)))
 
 
   (it "Solves #3"
-       (should= 2 (euler-3 4))
-       (should= 13 (euler-3 13))
-       (should= 5 (euler-3 10))
-       (should= 2 (euler-3 2))
-       (should= 1 (euler-3 1))
-       (should= 1 (euler-3 -1))
-       (should= 6857 (euler-3 600851475143))
-       )
-      )
+    (should= 1 (euler-3 0))
+    (should= 1 (euler-3 1))
+    (should= 1 (euler-3 -1))
+    (should= 2 (euler-3 2))
+    (should= 2 (euler-3 4))
+    (should= 5 (euler-3 10))
+    (should= 13 (euler-3 13))
+    (should= 6857 (euler-3 600851475143))))
 
 (run-specs)
--- src/euler/level1/problem003.clj
+++ src/euler/level1/problem003.clj
@@ -1,17 +1,15 @@
 (ns euler.level1.problem003)
 
 (defn smallest-prime-factor [n]
-      (if (< n 2)
-        1
-        (loop [factor 2]
-          (if (= (mod n factor) 0)
-            factor
-            (recur (inc factor))))))
+  (loop [factor 2]
+    (cond (< n 2) 1
+          (= (mod n factor) 0) factor
+          :else (recur (inc factor)))))
 
 (defn euler-3 [n]
-      (loop [n n
-             largest-factor-of-n 1]
-        (if-not (> n 1)
-          largest-factor-of-n
-          (let [factor (smallest-prime-factor n)]
-            (recur (/ n factor) factor)))))
+  (loop [n n
+         largest-factor-of-n 1]
+    (if-not (> n 1)
+      largest-factor-of-n
+      (let [factor (smallest-prime-factor n)]
+        (recur (/ n factor) factor)))))
