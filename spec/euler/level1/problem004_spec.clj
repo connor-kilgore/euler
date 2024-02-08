d0b3f20006042919b4bd740293c01eb6022a28be
Connor Kilgore <connor_kilgore@Connors-Mac-mini.local>
2024-02-08 13:54:19 -0700

    cleaned up #4

--- spec/euler/level1/problem004_spec.clj
+++ spec/euler/level1/problem004_spec.clj
@@ -8,22 +8,25 @@
 (describe "Euler Problem #4"
 
   (it "checks if number is a palindrome"
-    (should (is-palindrome? 1))
-    (should (is-palindrome? 2))
-    (should (is-palindrome? 121))
-    (should (is-palindrome? 12321))
-    (should (is-palindrome? 123321))
-    (should-not (is-palindrome? 122))
-    (should-not (is-palindrome? 12231221))
-    (should-not (is-palindrome? 12))
-    )
+    (should (is-palindrome? "1"))
+    (should (is-palindrome? "2"))
+    (should (is-palindrome? "121"))
+    (should (is-palindrome? "12321"))
+    (should (is-palindrome? "123321"))
+    (should-not (is-palindrome? "122"))
+    (should-not (is-palindrome? "12231221"))
+    (should-not (is-palindrome? "12")))
 
   (it "Solves #4"
-    (should= 9009 (euler-4 99))
-    (should= 906609 (euler-4 999))
-    (should= 9 (euler-4 10))
     (should= 1 (euler-4 1))
-    )
+    (should= 4 (euler-4 2))
+    (should= 9 (euler-4 3))
+    (should= 9 (euler-4 10))
+    (should= nil (euler-4 -1))
+    (should= 0 (euler-4 0))
+    (should= 121 (euler-4 12))
+    (should= 9009 (euler-4 99))
+    (should= 906609 (euler-4 999)))
   )
 
 (run-specs)
--- src/euler/level1/problem004.clj
+++ src/euler/level1/problem004.clj
@@ -1,18 +1,17 @@
 (ns euler.level1.problem004)
 
 (defn is-palindrome? [n]
-  (let [str-n (str n)]
-    (loop [start-index 0 end-index (- (count str-n) 1)]
-      (cond (>= start-index end-index) true
-            (not (= (get str-n start-index) (get str-n end-index))) false
-            :else (recur (inc start-index) (dec end-index))))))
+  (cond (<= (count n) 1) true
+        (= (first n) (last n)) (is-palindrome? (subs n 1 (dec (count n))))
+        :else false))
 
 (defn euler-4 [n]
   (loop [inner-loop-num n
          outer-loop-num n
          largest-multiple 0]
     (let [current-multiple (* inner-loop-num outer-loop-num)]
-      (cond (= outer-loop-num 0) largest-multiple
+      (cond (< n 0) nil
+            (= outer-loop-num 0) largest-multiple
             (= inner-loop-num 0) (recur n (dec outer-loop-num) largest-multiple)
-            (and (is-palindrome? current-multiple) (> current-multiple largest-multiple)) (recur (dec inner-loop-num) outer-loop-num current-multiple)
+            (and (is-palindrome? (str current-multiple)) (> current-multiple largest-multiple)) (recur (dec inner-loop-num) outer-loop-num current-multiple)
             :else (recur (dec inner-loop-num) outer-loop-num largest-multiple)))))
