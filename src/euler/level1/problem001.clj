212ff264159f2f8eb4822f43688957b4d9adc2ee
Connor Kilgore <connor_kilgore@Connors-Mac-mini.local>
2024-02-08 12:06:29 -0700

    Rewrote euler-1 much better

--- src/euler/level1/problem001.clj
+++ src/euler/level1/problem001.clj
@@ -3,9 +3,5 @@
 (defn multiple-of-n? [val n] (if (= val 0) false (zero? (mod val n))))
 
 (defn euler-1 [n]
-  (loop [i 1
-         sum-of-multiples 0]
-    (cond (>= i n) sum-of-multiples
-          (or (multiple-of-n? i 3) (multiple-of-n? i 5))
-          (recur (inc i) (+ sum-of-multiples i))
-          :else (recur (inc i) sum-of-multiples))))
+  (reduce (fn [sum i] (if (or (multiple-of-n? i 3) (multiple-of-n? i 5)) (+ sum i) sum))
+    0 (range n)))
--- src/euler/level1/problem002.clj
+++ src/euler/level1/problem002.clj
@@ -1,23 +1,7 @@
 (ns euler.level1.problem002)
 
 (defn euler-2 [n]
-      ; loop up to n starting with firstVal at 1 and secVal at 2
-      (loop [index 2
-             totalEven 0
-             firstVal 1
-             secVal 2]
-
-            (if (> index n)
-              totalEven
-            (do
-              ;; check if secVal is even
-              (if (even? secVal)
-                ;; recur with totalEven increased
-                (recur (inc index) (+ totalEven secVal) secVal (+ firstVal secVal))
-                ;; recur without totalEven increased
-                (recur (inc index) totalEven secVal (+ firstVal secVal))
-                )
-              )
-            )
-          )
-  )
+  (loop [index 2 totalEven 0 firstVal 1 secVal 2]
+    (cond (> index n) totalEven
+          (even? secVal) (recur (inc index) (+ totalEven secVal) secVal (+ firstVal secVal))
+          :else (recur (inc index) totalEven secVal (+ firstVal secVal)))))
