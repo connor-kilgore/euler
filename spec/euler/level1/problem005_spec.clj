4b32b935ce0fd451bc735cc405466bffbdc0c883
Connor Kilgore <connor_kilgore@Connors-Mac-mini.local>
2024-02-09 14:29:04 -0700

    problem 5 is now O(n) and runs near instant

--- spec/euler/level1/problem005_spec.clj
+++ spec/euler/level1/problem005_spec.clj
@@ -5,47 +5,70 @@
 
 ; https://projecteuler.net/problem=5
 
-(describe "Euler Problem #5"
+(describe "how a list of prime factors are retrieved 1-n and they are
+merged with the highest frequency of values overwriting."
 
- (it "moves index 1 integer closer to n"
-     (should= 2 (move-index-towards-n 1 2))
-     (should= 2 (move-index-towards-n 1 3))
-     (should= 3 (move-index-towards-n 2 3))
-     (should= 1 (move-index-towards-n 2 1))
-     (should= 1 (move-index-towards-n 2 -1))
-     (should= 999 (move-index-towards-n 1000 0))
-     )
+  (it "will return a prime number input"
+    (should= 2 (get-smallest-prime-factor 2))
+    (should= 3 (get-smallest-prime-factor 3)))
+  (it "will return nil for a number less than 2"
+    (should= nil (get-smallest-prime-factor 1))
+    (should= nil (get-smallest-prime-factor 0)))
+  (it "will return the smallest factor of a non-prime integer"
+    (should= 2 (get-smallest-prime-factor 4))
+    (should= 2 (get-smallest-prime-factor 10)))
 
-  (it "checks if a number is divisible by another number"
-      (should (divisible-by-num? 1 1))
-      (should-not (divisible-by-num? 1 2))
-      (should (divisible-by-num? 2 2))
-      (should (divisible-by-num? 2 0))
-      (should (divisible-by-num? 0 2))
-      (should-not (divisible-by-num? -1 2))
-      (should (divisible-by-num? 2 -1))
-      (should (divisible-by-num? 493024932 2))
-      )
+  (it "will return a map with both maps merged, overwriting the highest values"
+    (should= {2 2} (merge-maps-with-highest-values {2 1} {2 2}))
+    (should= {2 2, 3 1} (merge-maps-with-highest-values {2 1, 3 1} {2 2}))
+    (should= {2 2, 3 2} (merge-maps-with-highest-values {2 1, 3 2} {2 2, 3 1}))
+    )
 
-  (it "checks if number is divisible by 1-n numbers"
-      (should (is-divisible-by-n-nums? 1 1))
-      (should (is-divisible-by-n-nums? 2 2))
-      (should-not (is-divisible-by-n-nums? 3 2))
-      (should-not (is-divisible-by-n-nums? 4 4))
-      (should-not (is-divisible-by-n-nums? 1 3))
-      (should-not (is-divisible-by-n-nums? -1 3))
-      (should (is-divisible-by-n-nums? 2520 10))
-      )
+  (it "will merge all the maps together into 1 of highest frequency of each key"
+    (should= {2 1} (get-map-with-highest-frequency-vals 2))
+    (should= {2 2, 3 1, 5 1} (get-map-with-highest-frequency-vals 5))
+    (should= {2 3, 3 2, 5 1, 7 1} (get-map-with-highest-frequency-vals 10))
+    )
 
-  (it "Solves #5"
+  (it "will return numbers if single prime numbers"
+    (should= [2] (prime-factors 2))
+    (should= [3] (prime-factors 3))
+    (should= [13] (prime-factors 13)))
+  (it "will return nil for numbers less than 2"
+    (should= [] (prime-factors 1))
+    (should= [] (prime-factors 0)))
+
+  (it "will return every prime factor of non-primes"
+    (should= [2 3] (prime-factors 6))
+    (should= [2 5] (prime-factors 10))
+    (should= [2 2 5] (prime-factors 20))
+    )
+
+  (it "will return a list of maps stating the frequency of each factor from 1-n"
+    (should= '({2 1}) (get-map-of-frequencies-of-factors 2))
+    (should= '({2 1} {3 1} {2 2} {5 1}) (get-map-of-frequencies-of-factors 5))
+    (should= '() (get-map-of-frequencies-of-factors 0))
+    (should= '({2 1} {3 1} {2 2} {5 1} {2 1 3 1} {7 1} {2 3} {3 2} {2 1 5 1}) (get-map-of-frequencies-of-factors 10))
+    )
+
+  (it "Will return value if 1 or 2 is entered"
+    (should= 1 (euler-5 1))
+    (should= 2 (euler-5 2)))
+
+  (it "Will return nil if value below 1"
+    (should= nil (euler-5 0))
+    (should= nil (euler-5 -1)))
+
+  (it "Will return smallest integer divisible by all 1-n if otherwise"
     (should= 1 (euler-5 1))
     (should= 2 (euler-5 2))
+    (should= nil (euler-5 -1))
     (should= 6 (euler-5 3))
     (should= 12 (euler-5 4))
     (should= 2520 (euler-5 10))
-    (should= 1 (euler-5 -1))
-      ;(should= 232792560 (euler-5 20))
-      )
+    (should= 232792560 (euler-5 20))
+    (should= 5342931457063200 (euler-5 40))
+    )
   )
 
 (run-specs)
--- src/euler/level1/problem005.clj
+++ src/euler/level1/problem005.clj
@@ -1,28 +1,27 @@
 (ns euler.level1.problem005)
 
-(defn move-index-towards-n [index n]
-      (if (< index n)
-        (inc index)
-        (if (> index n) (dec index) index)
-        ))
+(defn get-smallest-prime-factor [n]
+  (first (filter (fn [x] (= (mod n x) 0)) (range 2 (inc n)))))
 
-(defn divisible-by-num? [numerator denominator]
-      ; skip 0-denominator by returning true
-      (if (= denominator 0)
-        true
-        (= (mod numerator denominator) 0)))
+(defn prime-factors [n]
+  (loop [n n
+         factors []]
+    (let [factor (get-smallest-prime-factor n)]
+      (if (= factor nil) factors (recur (/ n factor) (conj factors factor))))))
 
+(defn merge-maps-with-highest-values [map1 map2]
+  (merge-with #(if (> %1 %2) %1 %2) map1 map2)
+  )
 
-(defn is-divisible-by-n-nums? [number n]
-      (loop [index 0]
-        (if (divisible-by-num? number index)
-          (if (= index n)
-            true
-            (recur (move-index-towards-n index n)))
-          false)))
+(defn get-map-of-frequencies-of-factors [n]
+  (map frequencies (map prime-factors (range 2 (inc n))))
+  )
+
+(defn get-map-with-highest-frequency-vals [n]
+  (reduce merge-maps-with-highest-values (get-map-of-frequencies-of-factors n)))
 
 (defn euler-5 [n]
-  (loop [index 1]
-    (if (is-divisible-by-n-nums? index n)
-      index
-      (recur (inc index)))))
+  (cond (< n 1) nil
+        (< n 2) n
+        :else (reduce * (for [[key value] (get-map-with-highest-frequency-vals n)] (int (Math/pow key value))))
+        ))
