5d88821d56bf0a26cd84195f5ea5b5119432e5b8
Connor Kilgore <connor_kilgore@Connors-Mac-mini.local>
2024-02-13 10:28:09 -0700

    fixed up 9

--- spec/euler/level1/problem009_spec.clj
+++ spec/euler/level1/problem009_spec.clj
@@ -10,10 +10,9 @@
   (it "checks if 3 values are a pythagorean triplet"
     (should (is-pythagorean-triplet? 3 4 5))
     (should (is-pythagorean-triplet? 5 12 13))
-    (should (is-pythagorean-triplet? 9 40 41))
     (should-not (is-pythagorean-triplet? 9 40 40))
     (should-not (is-pythagorean-triplet? 1 1 1))
-  )
+    )
 
   (it "Solves #9"
     (should= 60 (euler-9 12))
@@ -21,7 +20,7 @@
     (should= 31875000 (euler-9 1000))
     (should= nil (euler-9 500))
     (should= nil (euler-9 1))
+    )
   )
-)
 
 (run-specs)
--- src/euler/level1/problem009.clj
+++ src/euler/level1/problem009.clj
@@ -1,27 +1,13 @@
 (ns euler.level1.problem009)
 
 (defn is-pythagorean-triplet? [a b c]
-  (and (and (< a b) (< b c)) (= (+ (* a a) (* b b)) (* c c)))
-)
+  (and (and (< a b) (< b c)) (= (+ (* a a) (* b b)) (* c c))))
 
 (defn euler-9 [n]
   (loop [a 1
          b (+ a 1)]
-
     (let [c (- n a b)]
-      (if (is-pythagorean-triplet? a b c)
-        (* a b c)
-        (do
-          (if (< b (quot (- n a) 2))
-            (recur a (inc b))
-            (do
-              (if (< a (quot n 3))
-                (recur (inc a) (+ a 2))
-                nil
-                )
-              )
-            )
-          )
-        ))
-    )
-)
+      (cond (is-pythagorean-triplet? a b c) (* a b c)
+            (< b (quot (- n a) 2)) (recur a (inc b))
+            (< a (quot n 3)) (recur (inc a) (+ a 2))
+            :else nil))))
