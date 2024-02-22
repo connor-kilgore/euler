204d9849edb3d7e3a53cc1d90b444ebda91c71c9
Connor Kilgore <connor_kilgore@Connors-Mac-mini.local>
2024-02-22 13:49:21 -0700

    fixed Euler 12

--- src/euler/level1/problem012.clj
+++ src/euler/level1/problem012.clj
@@ -1,20 +1,23 @@
 (ns euler.level1.problem012)
 
-(defn get-factors [n]
-  (filter #(= (mod n %) 0) (range 1 (inc n))))
+(:require Math)
 
-(defn get-triangle-number-sum [n]
-  (apply + (range (inc n))))
+(defn get-num-factors-of-n
+  ([n] (if (< n 1) 0 (get-num-factors-of-n n 1 0)))
+  ([n i count]
+   (cond (> i (Math/sqrt n)) count
+         (not (= (mod n i) 0)) (recur n (inc i) count)
+         (= (/ n i) i) (recur n (inc i) (inc count))
+         :else (recur n (inc i) (+ count 2)))))
 
 (defn has-more-divisors-than-n? [triangle-num n]
   (-> triangle-num
-      (get-factors)
-      (count)
+      (get-num-factors-of-n)
       (>= n)))
 
 (defn euler-12
   ([n] (euler-12 2 1 n))
-  ([next-num triangle-num n]
+  ([layer triangle-num n]
    (if (has-more-divisors-than-n? triangle-num n)
      triangle-num
-     (recur (inc next-num) (+ triangle-num next-num) n))))
+     (recur (inc layer) (+ triangle-num layer) n))))
